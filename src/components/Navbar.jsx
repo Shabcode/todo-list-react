30584cc1a6e85ce9386c0420baf150cbc1c409d1
Lucas Shahbazi <lucasshahbazi@yahoo.de>
2025-02-04 16:31:30 +0100

    Implemented basic Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
+      <h1>My Todo List</h1>
+    </nav>
+  );
+}
