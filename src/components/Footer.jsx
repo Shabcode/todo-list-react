13c519a90bc2f3c9b7dd910829b9b22f3b47e6f4
Lucas Shahbazi <lucasshahbazi@yahoo.de>
2025-02-04 16:29:01 +0100

    Implemented basic Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="Footer">
+            <a href="https://github.com/Shabcode/todo-list-react" target="_blank" rel="noopener noreferrer">
+            GitHub Repository
+            </a>
+        </footer>
+    );
+};
+
+export default Footer;
